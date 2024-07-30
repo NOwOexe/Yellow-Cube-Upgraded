ad2d341fc76b1b3e292d677ce6f87a5d1b0ac4b2
N <namouwuexe@gmail.com>
2024-07-30 22:33:21 +0700

    Update

--- main.py
+++ main.py
@@ -14,7 +14,28 @@ class Game():
         self.background = pygame.transform.scale(self.background, (800, 600))
         self.clock = pygame.time.Clock()
         self.score = 0
+        self.icon = pygame.image.load("icon.png")
+        pygame.display.set_icon(self.icon)
         self.font = pygame.font.Font("SuperPixel-m2L8j.ttf", 20)
+        
+    def start_menu(self):
+        run = True
+        while run:
+            self.screen.fill((0, 0, 0))
+            self.draw_text = self.font.render(f"Yellow Cube", True, (255, 255, 0))
+            self.draw_text2 = self.font.render(f"Press Spacebar To Continue.", True, (255, 255, 0))
+            text_w, text_h = self.draw_text.get_width(), self.draw_text.get_height() + 150
+            text2_w, text2_h = self.draw_text2.get_width(), self.draw_text2.get_height()
+            self.screen.blit(self.draw_text, ((self.screen.get_width() - text_w) // 2, (self.screen.get_height() - text_h) // 2))
+            self.screen.blit(self.draw_text2, ((self.screen.get_width() - text2_w) // 2, (self.screen.get_height() - text2_h) // 2))
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    run = False
+            key = pygame.key.get_pressed()
+            if key[pygame.K_SPACE]:
+                run = False
+                game.run()
+            pygame.display.update()
         
     def run(self):
         run = True
@@ -29,7 +50,7 @@ class Game():
             self.screen.blit(self.background, (0, 0))
             self.player.draw_player(400, 300)
             self.player.move(delta_time)
-            self.draw_text = self.font.render(f"Score: {self.score}", True, (0, 0, 0))
+            self.draw_text = self.font.render(f"Score: {self.score}", True, (255, 255, 0))
             self.screen.blit(self.draw_text, (20, 20))
                 
             pygame.display.update()
@@ -38,4 +59,4 @@ class Game():
         
 if __name__ == "__main__":
     game = Game()
-    game.run()
+    game.start_menu()
